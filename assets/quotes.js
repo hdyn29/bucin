7ffbdc9ce867dcfe3f5239764b740d66d61b9ae3
hdyn29 <74176019+hdyn29@users.noreply.github.com>
2020-11-09 19:43:40 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,8 @@
 // prettier-ignore
 export default [
-    { author: "avrill.v", quotes: "Whatever happens, keep breathing" },
-    { author: "antjedtta_", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
-    { author: "standinshd", quotes: "Hey! I wuv chuuuu! UwU" },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I may not with you everyday, but I love you everyday." },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I'm sorry for loving you." },
+    { author: "Hadiyan", quotes: "Melihat senyummu adalah kebahagiaan bagiku. Sementara, memilikimu adalah hadiah yang paling indah dalam hidupku" },
+    { author: "Hadiyan", quotes: "Cukup jaringan aja yang hilang, kamu jangan" },
+    { author: "Hadiyan", quotes: "Fokus ke kamu aja, gak mau ke yang lain" },
+    { author: "Hadiyan", quotes: "Aku tanpamu bagaikan ambulans tanpa 'wiuw wiuw wiuw OwO" },
+    { author: "Hadiyan", quotes: "Hey, I love you <3" },
 ];
