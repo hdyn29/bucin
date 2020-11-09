1fe247ca92ff4f240d207753b15e4fa3ad264dd1
hdyn29 <74176019+hdyn29@users.noreply.github.com>
2020-11-09 20:02:50 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -3,6 +3,6 @@ export default [
     { author: "Hadiyan", quotes: "Melihat senyummu adalah kebahagiaan bagiku. Sementara, memilikimu adalah hadiah yang paling indah dalam hidupku" },
     { author: "Hadiyan", quotes: "Cukup jaringan aja yang hilang, kamu jangan" },
     { author: "Hadiyan", quotes: "Fokus ke kamu aja, gak mau ke yang lain" },
-    { author: "Hadiyan", quotes: "Aku tanpamu bagaikan ambulans tanpa 'wiuw wiuw wiuw OwO" },
+    { author: "Hadiyan", quotes: "Aku tanpamu bagaikan ambulans tanpa 'wiuw wiuw wiuw'" },
     { author: "Hadiyan", quotes: "Hey, I love you <3" },
 ];
