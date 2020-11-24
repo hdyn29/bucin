80339db6200a1e070efeb900aee2b184a6d446ce
hdyn29 <74176019+hdyn29@users.noreply.github.com>
2020-11-24 18:28:20 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,8 +1,8 @@
 // prettier-ignore
 export default [
-    { author: "Hadiyan", quotes: "tetep semangat walau hidup kadang suka ga adil" },
-    { author: "Hadiyan", quotes: "jaku yakin kamu pasti bisa kok :)" },
-    { author: "Hadiyan", quotes: "hey, aku mau bilang kalo orang Bogor itu sok asik xixixi" },
-    { author: "Hadiyan", quotes: "when you love what you have, you have everything you need" },
-    { author: "Hadiyan", quotes: "without the dark we'd never see the stars" },
+    { author: "Hadiyan", quotes: "..." },
+    { author: "Hadiyan", quotes: "..." },
+    { author: "Hadiyan", quotes: "..." },
+    { author: "Hadiyan", quotes: "..." },
+    { author: "Hadiyan", quotes: "..." },
 ];
